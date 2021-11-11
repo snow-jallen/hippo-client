fd1292f6d512cf413e7d3a574969351d8a4c56a5
Jonathan Allen <jallen@snow.edu>
2021-11-11 08:14:33 -0700

    Initial version

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HippoClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serverAddress = args.Length > 0 ? args[0] : getServerAddress();
+            var playerName = args.Length > 1 ? args[1] : getPlayerName();
+            var gameClient = await GameClient.CreateClientAsync(serverAddress, playerName);
+
+            Console.WriteLine("Congratulations, you are connected!\nNow use the arrow keys to eat all the things!");
+            await makeMoves(gameClient);
+        }
+
+        private static string getServerAddress()
+        {
+            var defaultAddresses = new[]{
+                "https://hungry-hungry-hippos.herokuapp.com",
+                "http://144.17.s48.37"
+            };
+            var input = getString($"To which server would you like to connect?\n1) {defaultAddresses[0]}\n2) {defaultAddresses[1]} (GRSC 143 instructor PC)\n3) Other (you enter your own)", 1);
+            switch (input)
+            {
+                case "0":
+                case "1":
+                    var index = int.Parse(input) - 1;
+                    return defaultAddresses[index];
+                default:
+                    return getString("Please enter the custom server address:", 4);
+            }
+        }
+
+        private static string getString(string prompt, int minLength)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n" + prompt);
+                var input = Console.ReadLine();
+                if (input.Length >= minLength)
+                {
+                    return input;
+                }
+                Console.WriteLine($"Please enter at least {minLength} character{(minLength > 1 ? "s" : "")}");
+            }
+        }
+
+        private static string getPlayerName()
+        {
+            return getString("Please enter your name", 3);
+        }        
+
+        private static async Task makeMoves(GameClient gameClient)
+        {
+            while(true)
+            {
+                var k = Console.ReadKey();
+                switch(k.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        await gameClient.MoveLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        await gameClient.MoveRight();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        await gameClient.MoveUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        await gameClient.MoveDown();
+                        break;
+                }
+            }
+        }
+    }
+
+    public class GameClient
+    {
+        private readonly string serverAddress;
+        private readonly string playerName;
+        HttpClient httpClient = new HttpClient();
+
+        private GameClient(string serverAddress, string playerName)
+        {
+            this.serverAddress = serverAddress;
+            this.playerName = playerName;
+        }
+
+        public string Token{get;private set;}
+
+        public async Task MoveLeft() => await httpClient.GetAsync($"{serverAddress}/move/left?token={Token}");
+        public async Task MoveRight() => await httpClient.GetAsync($"{serverAddress}/move/right?token={Token}");
+        public async Task MoveUp() => await httpClient.GetAsync($"{serverAddress}/move/up?token={Token}");
+        public async Task MoveDown() => await httpClient.GetAsync($"{serverAddress}/move/down?token={Token}");
+
+        public static async Task<GameClient> CreateClientAsync(string serverAddress, string playerName)
+        {
+            var client = new GameClient(serverAddress, playerName);
+
+            try
+            {
+                var response = await client.httpClient.GetStringAsync($"{serverAddress}/join?userName={playerName}");
+                client.Token = response;
+            }
+            catch (Exception ex)
+            {
+                throw new UnableToConnectToServerException("Unable to connect to server.", ex);
+            }
+
+            return client;
+        }
+    }
+}
---
+++ UnableToConnectToServerException.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace HippoClient
+{
+    [Serializable]
+    internal class UnableToConnectToServerException : Exception
+    {
+        public UnableToConnectToServerException()
+        {
+        }
+
+        public UnableToConnectToServerException(string message) : base(message)
+        {
+        }
+
+        public UnableToConnectToServerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnableToConnectToServerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
