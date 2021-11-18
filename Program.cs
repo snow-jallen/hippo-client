bae9c6bbd3c9008ae36f3aaf4a3dbf1a846e80e7
Jonathan Allen <jallen@snow.edu>
2021-11-18 08:41:00 -0700

    Add smarts to re-join existing game

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,12 +9,48 @@ namespace HippoClient
     {
         static async Task Main(string[] args)
         {
-            var serverAddress = args.Length > 0 ? args[0] : getServerAddress();
-            var playerName = args.Length > 1 ? args[1] : getPlayerName();
-            var gameClient = await GameClient.CreateClientAsync(serverAddress, playerName);
+            GameClient gameClient = null;
+
+            if (GameClient.IsPreviousConnectionAvailable)
+            {
+                var prevConnection = GameClient.PreviousConnection;
+                Console.WriteLine($"A previous connection exists to {prevConnection.ServerUrl} on {prevConnection.LastConnected}");
+                var usePrevious = getBool("Would you like to use that connection?");
+                if(usePrevious)
+                {
+                    gameClient = GameClient.ReCreateClient(prevConnection);
+                }
+                else
+                {
+                    if(getBool("Do you want to delete that saved connection info?"))
+                    {
+                        GameClient.DeletePreviousConnection();
+                    }
+                }
+            }
+
+            if (gameClient == null)
+            {
+                var serverAddress = args.Length > 0 ? args[0] : getServerAddress();
+                var playerName = args.Length > 1 ? args[1] : getPlayerName();
+                gameClient = await GameClient.CreateClientAsync(serverAddress, playerName);
+            }
 
             Console.WriteLine("Congratulations, you are connected!\nNow use the arrow keys to eat all the things!");
             await makeMoves(gameClient);
+        }
+
+        private static bool getBool(string prompt)
+        {
+            while (true)
+            {
+                Console.WriteLine(prompt + " (Y/N)");
+                var input = Console.ReadLine().ToUpper();
+                if (input.Contains("Y"))
+                    return true;
+                if (input.Contains("N"))
+                    return false;
+            }
         }
 
         private static string getServerAddress()
@@ -51,14 +88,14 @@ namespace HippoClient
         private static string getPlayerName()
         {
             return getString("Please enter your name", 3);
-        }        
+        }
 
         private static async Task makeMoves(GameClient gameClient)
         {
-            while(true)
+            while (true)
             {
                 var k = Console.ReadKey();
-                switch(k.Key)
+                switch (k.Key)
                 {
                     case ConsoleKey.LeftArrow:
                         await gameClient.MoveLeft();
@@ -79,6 +116,7 @@ namespace HippoClient
 
     public class GameClient
     {
+        private const string PreviousConnectionFile = "previousConnection.txt";
         private readonly string serverAddress;
         private readonly string playerName;
         HttpClient httpClient = new HttpClient();
@@ -89,7 +127,7 @@ namespace HippoClient
             this.playerName = playerName;
         }
 
-        public string Token{get;private set;}
+        public string Token { get; private set; }
 
         public async Task MoveLeft() => await httpClient.GetAsync($"{serverAddress}/move/left?token={Token}");
         public async Task MoveRight() => await httpClient.GetAsync($"{serverAddress}/move/right?token={Token}");
@@ -104,6 +142,8 @@ namespace HippoClient
             {
                 var response = await client.httpClient.GetStringAsync($"{serverAddress}/join?userName={playerName}");
                 client.Token = response;
+
+                saveConnectionInfo(serverAddress, response);
             }
             catch (Exception ex)
             {
@@ -112,5 +152,45 @@ namespace HippoClient
 
             return client;
         }
+
+        internal static GameClient ReCreateClient(PreviousConnection prevConnection)
+        {
+            var client = new GameClient(prevConnection.ServerUrl, "undetermined");
+            client.Token = prevConnection.Token;
+            return client;
+        }
+
+        public static bool IsPreviousConnectionAvailable => File.Exists(PreviousConnectionFile);
+
+        public static PreviousConnection PreviousConnection
+        {
+            get
+            {
+                if (File.Exists(PreviousConnectionFile))
+                {
+                    var json = File.ReadAllText(PreviousConnectionFile);
+                    var connectionInfo = System.Text.Json.JsonSerializer.Deserialize<PreviousConnection>(json);
+                    return connectionInfo;
+                }
+                return null;
+            }
+        }
+
+        private static void saveConnectionInfo(string serverAddress, string response)
+        {
+            var connectionInfo = new PreviousConnection(serverAddress, response, DateTime.Now);
+            var json = System.Text.Json.JsonSerializer.Serialize(connectionInfo);
+            File.WriteAllText(PreviousConnectionFile, json);
+        }
+
+        internal static void DeletePreviousConnection()
+        {
+            if (File.Exists(PreviousConnectionFile))
+            {
+                File.Delete(PreviousConnectionFile);
+            }
+        }
     }
+
+    public record PreviousConnection(string ServerUrl, string Token, DateTime LastConnected);
 }
