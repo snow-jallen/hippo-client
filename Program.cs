26d866cf85b77fd93d7902ad1b6ac7e14a2000d1
Jonathan Allen <jallen@snow.edu>
2021-11-22 07:38:14 -0700

    Naming conventions

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace HippoClient
         {
             var defaultAddresses = new[]{
                 "https://hungry-hungry-hippos.herokuapp.com",
-                "http://144.17.s48.37"
+                "http://144.17.s48.37",
+                "http://localhost:5291"
             };
             var input = getString($"To which server would you like to connect?\n1) {defaultAddresses[0]}\n2) {defaultAddresses[1]} (GRSC 143 instructor PC)\n3) Other (you enter your own)", 1);
             switch (input)
@@ -51,26 +52,26 @@ namespace HippoClient
         private static string getPlayerName()
         {
             return getString("Please enter your name", 3);
-        }        
+        }
 
         private static async Task makeMoves(GameClient gameClient)
         {
-            while(true)
+            while (true)
             {
                 var k = Console.ReadKey();
-                switch(k.Key)
+                switch (k.Key)
                 {
                     case ConsoleKey.LeftArrow:
-                        await gameClient.MoveLeft();
+                        await gameClient.MoveLeftAsync();
                         break;
                     case ConsoleKey.RightArrow:
-                        await gameClient.MoveRight();
+                        await gameClient.MoveRightAsync();
                         break;
                     case ConsoleKey.UpArrow:
-                        await gameClient.MoveUp();
+                        await gameClient.MoveUpAsync();
                         break;
                     case ConsoleKey.DownArrow:
-                        await gameClient.MoveDown();
+                        await gameClient.MoveDownAsync();
                         break;
                 }
             }
@@ -89,12 +90,12 @@ namespace HippoClient
             this.playerName = playerName;
         }
 
-        public string Token{get;private set;}
+        public string Token { get; private set; }
 
-        public async Task MoveLeft() => await httpClient.GetAsync($"{serverAddress}/move/left?token={Token}");
-        public async Task MoveRight() => await httpClient.GetAsync($"{serverAddress}/move/right?token={Token}");
-        public async Task MoveUp() => await httpClient.GetAsync($"{serverAddress}/move/up?token={Token}");
-        public async Task MoveDown() => await httpClient.GetAsync($"{serverAddress}/move/down?token={Token}");
+        public async Task MoveLeftAsync() => await httpClient.GetAsync($"{serverAddress}/move/left?token={Token}");
+        public async Task MoveRightAsync() => await httpClient.GetAsync($"{serverAddress}/move/right?token={Token}");
+        public async Task MoveUpAsync() => await httpClient.GetAsync($"{serverAddress}/move/up?token={Token}");
+        public async Task MoveDownAsync() => await httpClient.GetAsync($"{serverAddress}/move/down?token={Token}");
 
         public static async Task<GameClient> CreateClientAsync(string serverAddress, string playerName)
         {
@@ -107,7 +108,7 @@ namespace HippoClient
             }
             catch (Exception ex)
             {
-                throw new UnableToConnectToServerException("Unable to connect to server.", ex);
+                throw new Exception("Unable to connect to server.", ex);
             }
 
             return client;
